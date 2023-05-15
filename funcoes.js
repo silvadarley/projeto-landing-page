f185dee902946572d2453aae770fa3f445e82183
silvadarley <133307393+silvadarley@users.noreply.github.com>
2023-05-15 19:32:06 -0300

    Update funcoes.js

--- funcoes.js
+++ funcoes.js
@@ -7,6 +7,10 @@ var setaDireita = window.document.getElementById("setaDireita")
 function rolarDireita() {
     Leonardo.style = "display: none"
     Samantha.style = "display: flex"
+    
+    setaEsquerda.style = "margin-top: 25%"
+    setaDireita.style = "margin-top: 25%"
+    
     setaDireita.style = "display: none"
     setaEsquerda.style = "display: flex"
 }
@@ -15,6 +19,10 @@ function rolarEsqerda(){
     Samantha.style = "display: none"
     Leonardo.style = "display: flex"
     /*Bruna.style = "display: nome"*/
+    
+    setaEsquerda.style = "margin-top: 25%"
+    setaDireita.style = "margin-top: 25%"
+    
     setaDireita.style = "display:flex"
     setaEsquerda.style = "display:none"
 }
